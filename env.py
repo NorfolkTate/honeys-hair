2fc6209a092b0e746c64ba131fedb2ed3b0c9e04
Amy Norfolk Tate <amynorfolk2@gmail.com>
2025-03-26 10:52:09 +0000

    add database and secret key

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+
+os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:ixdMmFnrK14u@ep-lingering-wildflower-a21sjs32.eu-central-1.aws.neon.tech/wick_ebook_halo_430270")
+os.environ.setdefault("SECRET_KEY", "nK7jGyRNd9e")
--- salon/settings.py
+++ salon/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile('env.py'):
+    import env
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)db$yt8ifw^p$4zk=mve%mn_)0d-sqdq0n(zt9h4ss+q*ppdkb'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,11 +77,15 @@ WSGI_APPLICATION = 'salon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
