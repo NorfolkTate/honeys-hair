1cf7cee9ae13d6f0a5e3bfa9d0a00c18eb6311fa
Amy Norfolk Tate <amynorfolk2@gmail.com>
2025-03-26 13:08:47 +0000

    add service model and link to booking model

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Booking
+from .models import Booking, Service
 
 admin.site.register(Booking)
+admin.site.register(Service)
--- booking/models.py
+++ booking/models.py
@@ -4,9 +4,20 @@ from django.db import models
 
 class Booking(models.Model):
     name = models.CharField(max_length=100)
-    service = models.CharField(max_length=100)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE) #code helpfully inspired by StackOverflow and referenced in Readme
     date = models.DateField()
     time = models.TimeField()
 
     def __str__(self):
         return f"{self.name} - {self.service} on {self.date} at {self.time}"
+    
+    
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
