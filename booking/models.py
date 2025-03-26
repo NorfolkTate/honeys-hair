cc296369ac408405e35ac0a6c0b17611ce7d9975
Amy Norfolk Tate <amynorfolk2@gmail.com>
2025-03-26 13:20:20 +0000

    fix error in order of service and booking models

--- booking/models.py
+++ booking/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Service(models.Model):
+    name = models.CharField(max_length=100) # code helpfully inspired by python programming tutorials and referenced in Readme
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
 class Booking(models.Model):
     name = models.CharField(max_length=100)
     service = models.ForeignKey(Service, on_delete=models.CASCADE) #code helpfully inspired by StackOverflow and referenced in Readme
@@ -10,14 +18,3 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.service} on {self.date} at {self.time}"
-    
-    
-
-
-class Service(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return self.name
