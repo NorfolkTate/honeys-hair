e322e55adc9dda662a81adc2a9634beaf04394b6
Amy Norfolk Tate <amynorfolk2@gmail.com>
2025-03-26 14:24:50 +0000

    add a view function to views.py to book appointments

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # code helpfully inspired by stackoverflow and referenced in Readme 
 
 # Create your views here.
+
+from .forms import BookingForm
+
+def book_appointment(request): 
+    if request.method == 'POST':
+        form = BookingForm(request.POST) # code helpfully provided by Stackoverflow and referenced in Readme
+        if form.is_valid():
+            form.save()
+            return redirect()
+    else:
+        form = BookingForm()
+    
+    return render(request)
