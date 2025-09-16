fe75ba1c8aca50d60e3e05bdc59218f1f5e9fc0a
Amy Norfolk Tate <amynorfolk2@gmail.com>
2025-09-16 11:42:12 +0100

    update all auth settings in settings

--- salon/settings.py
+++ salon/settings.py
@@ -50,11 +50,10 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-# allauth
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
-ACCOUNT_EMAIL_REQUIRED = False
-ACCOUNT_EMAIL_VERIFICATION = "none"
-ACCOUNT_USERNAME_REQUIRED = True
+# allauth updates
+ACCOUNT_LOGIN_METHODS = {"username", "email"}
+ACCOUNT_SIGNUP_FIELDS = ["email", "username*", "password1*", "password2*"]
+
 
 LOGIN_URL = "account_login"
 
